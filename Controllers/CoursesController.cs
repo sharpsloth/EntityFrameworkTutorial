2276e96407bb93fe9455a809f001488a8879735a
Clarisse Mendenhall <clarissemendenhall@gmail.com>
2020-06-27 00:56:07 -0500

    finished tutorial, need to add personalization

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -164,6 +164,23 @@ namespace ContosoUniversity.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewData["RowsAffected"] =
+                    await _context.Database.ExecuteSqlCommandAsync(
+                        "UPDATE Course SET Credits = Credits * {0}",
+                        parameters: multiplier);
+            }
+            return View();
+        }
 
         private bool CourseExists(int id)
         {
